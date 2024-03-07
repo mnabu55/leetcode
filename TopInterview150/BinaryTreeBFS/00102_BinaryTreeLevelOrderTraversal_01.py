49f0bbb563cfb6e03920164e42f4811c2f665435
Manabu Kiyota <agmk4655@gmail.com>
2024-03-07 19:04:16 +0900

    Create 00102_BinaryTreeLevelOrderTraversal_01.py

---
+++ TopInterview150/BinaryTreeBFS/00102_BinaryTreeLevelOrderTraversal_01.py
@@ -0,0 +1,39 @@
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        queue = [root]
+        res = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
+
+
+if __name__ == '__main__':
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    print(Solution().levelOrder(root))
+    
